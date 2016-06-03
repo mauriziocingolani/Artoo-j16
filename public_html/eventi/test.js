2c0278882b5eb48aa96278e849b4c5d00581ec17
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-06-03 15:55:27 +0200

    Eventi base 1

---
+++ public_html/eventi/test.js
@@ -0,0 +1,17 @@
+/**
+ * Funzione che fa qualcosa.
+ * <ul>
+ * <li>ciao</li>
+ * <li>ciao</li>
+ * <li>ciao</li>
+ * <li>ciao</li>
+ * </ul>
+ * @param string a
+ * @param {type} b
+ * @param {type} c
+ * @param {type} d
+ * @returns int Valore di qualcosa
+ */
+function pippo(a,b,c,d) {
+    
+}
